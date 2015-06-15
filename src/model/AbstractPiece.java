73abbda07d2b7cecb5cc94b9e219c2199eb5812f
denwou <alexis.neto@cpe.fr>
2015-06-15 13:36:45 +0200

    wx

--- src/model/AbstractPiece.java
+++ src/model/AbstractPiece.java
@@ -3,6 +3,13 @@ package model;
 public abstract class AbstractPiece implements Pieces {
 	private Couleur couleur;
 	private Coord coord;
+	private String name;
+	
+	public AbstractPiece(String name, Couleur couleur, Coord coord) {
+		this.name = name;
+		this.couleur = couleur;
+		this.coord = coord;
+	}
 
 	@Override
 	public int getX() {
@@ -16,23 +23,21 @@ public abstract class AbstractPiece implements Pieces {
 
 	@Override
 	public boolean move(int xFinal, int yFinal) {
-		return false;
+		return coord.coordonnees_valides(xFinal, yFinal);
 	}
 	
 	@Override
-	public boolean isMoveOk(int xFinal, int yFinal) {
-		// TODO Auto-generated method stub
-		return false;
-	}
+	abstract public boolean isMoveOk(int xFinal, int yFinal);
 
 	@Override
 	public boolean capture() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	@Override
-	abstract public String getName();
+	public String getName() {
+		return name;
+	}
 
 	@Override
 	public Couleur getCouleur() {
---
+++ src/model/Tour.java
@@ -0,0 +1,9 @@
+package model;
+
+public class Tour extends AbstractPiece {
+
+	public Tour() {
+	}
+
+	
+}
